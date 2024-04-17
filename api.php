622707b45c8567ae2098cfc84dcbd09c386bbec8
Siddeshwar <siddeshwar.work@gmail.com>
2024-04-17 12:59:00 +0530

    load and replace content

--- api.php
+++ api.php
@@ -2,13 +2,14 @@
 
 session_start();
 
-header('Content-Type: application/json');
 
 $_SESSION['people'] = [
     ['id' => 'a', 'name' => 'John doe', 'age' => 23]
 ];
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header('Content-Type: application/json');
+
     $data = file_get_contents("php://input");
     $body = json_decode($data, true);
     
@@ -16,5 +17,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     echo json_encode([ 'message' => 'User created', 'user' => $body ]);
 } else {
-    echo json_encode($_SESSION['people']);
-}
+    foreach($_SESSION['people'] as $person) {
+?>
+<div class="card">
+    <h3><?php echo $person['name'] ?></h3>
+    <p>Age: <?php echo $person['age'] ?></p>
+</div>
+<?php }}?>
