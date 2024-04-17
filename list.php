e264eb217ba4398514733e8e30c6ac699102ef06
Siddeshwar <siddeshwar.work@gmail.com>
2024-04-17 16:53:17 +0530

    bugfix

--- list.php
+++ list.php
@@ -2,28 +2,21 @@
 
 session_start();
 
-$_SESSION['people'] = [
-    ['id' => 'a', 'name' => 'John doe', 'age' => 23],
-    ['id' => 'b', 'name' => 'Kevin', 'age' => 15],
-    ['id' => 'c', 'name' => 'Luke', 'age' => 27],
-    ['id' => 'd', 'name' => 'Moo', 'age' => 35]
-];
+if(!isset($_SESSION['people'])) {
+    $_SESSION['people'] = [
+        ['id' => 'a', 'name' => 'John doe', 'age' => 23],
+        ['id' => 'b', 'name' => 'Kevin', 'age' => 15],
+        ['id' => 'c', 'name' => 'Luke', 'age' => 27],
+        ['id' => 'd', 'name' => 'Moo', 'age' => 35]
+    ];
+}
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    header('Content-Type: application/json');
+$people = $_SESSION['people'];
 
-    $data = file_get_contents("php://input");
-    $body = json_decode($data, true);
-    
-    array_push($_SESSION['people'], $body);
+if(isset($_GET['q']) && trim($_GET['q']) != '') {
+    $people = array_filter($people, fn($p) => str_contains(strtolower($p['name']), strtolower($_GET['q'])));
+}
 
-    echo json_encode([ 'message' => 'User created', 'user' => $body ]);
-} else {
-    $people = $_SESSION['people'];
-
-    if(isset($_GET['q']) && trim($_GET['q']) != '') {
-        $people = array_filter($people, fn($p) => str_contains(strtolower($p['name']), strtolower($_GET['q'])));
-    }
 ?>
 <table>
     <tr>
@@ -47,4 +40,3 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     </tr>
 <?php }?>
 </table>
-<?php } ?>
